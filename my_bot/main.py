72b9ae4b4782845708cd53ff15d3096edb95defa
AnnLinnden <annlinnden@gmail.com>
2025-05-08 14:31:41 +0600

    Обновлена система логирования

--- my_bot/main.py
+++ my_bot/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from logging.handlers import RotatingFileHandler
 from config import bot, dp, set_commands
 from handlers.user_handlers import user_router
 from handlers.admin_handlers import admin_router
@@ -7,8 +8,17 @@ from timing.payments import payment_router
 from db.storage import DatabaseManager
 
 database_manager = DatabaseManager()
-logging.basicConfig(level=logging.INFO)  # Включаем логирование: будем записывать логи событий и ошибок
+logging.basicConfig(level=logging.INFO,
+                    filename="app.log",  # Логи будут сохраняться в этот файл
+                    filemode="a",  # логи будут дозаписываться в файл
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
+handler = RotatingFileHandler(  # если файл с логами заполнится, создастся новый
+    "app.log",
+    maxBytes=5 * 1024 * 1024,  # размер файла логов - 5 MB
+    backupCount=3               # бот будет хранить 3 резервные копии
+)
+logger.addHandler(handler)
 
 
 async def main():
